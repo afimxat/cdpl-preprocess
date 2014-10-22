0b0c225b531717b27d0515eed21b35ed984ea50a
afimxat <toutku@gmail.com>
2014-10-22 15:35:46 +0300

    Revert "initial upload"
    This reverts commit ba4f0d8417d99066eaf84402a3fdf38b8f75f60a.

--- cdpl_preprocess/call_raw_data.m
+++
@@ -1,9 +0,0 @@
-function [raw_data] = call_raw_data(address_dir,address_file_name)
-%% defining EEG 
-    clear subjectdata
-% define the filenames, parameters and other information that is subject specific
-    subjectdata.dataset           = fullfile(address_dir,address_file_name);
-    raw_data=subjectdata.dataset;
-
-end
-
